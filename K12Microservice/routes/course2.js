e5610fc25811bd98942154834d4c3b5fca281b90
zshhhh <zhangsh0711@gmail.com>
2015-12-20 02:43:19 -0500

    K12

--- K12Microservice/routes/course2.js
+++ K12Microservice/routes/course2.js
@@ -3,12 +3,15 @@
  */
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongoskin');
-var db = mongo.db("mongodb://jingxiao:jingxiao@ds041154.mongolab.com:41154/course", {native_parser:true});
-var collectionName = "course2";
-var fs = require('fs');
-var logFile = './logfile.log';
 var async = require('async');
+var AWS = require('aws-sdk');
+var table = "K12StudentInfo";
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+var dynamodb = new AWS.DynamoDB();
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
 
 function getDateTime() {
     var date = new Date();
@@ -27,381 +30,170 @@ function getDateTime() {
 }
 
 //Add a new course
-router.post('/courses', function (req, res, next) {
-    db.collection(collectionName).find({"id": req.body.id}).toArray(function (err, result) {
+router.post('/students', function (req, res, next) {
+    var studentID = req.body.studentID;
+    var name = req.body.name;
+    if (studentID == null || name == null) {
+        res.send(JSON.stringify({ RET:402,status:"wrong JSON format" }));
+    }
+    var elementary_school = req.body.elementary_school;
+    var elementary_school_enrolling_date = req.body.elementary_school_enrolling_date;
+    var elementary_school_graduate_date = req.body.elementary_school_graduate_date;
+    var middle_school = req.body.middle_school;
+    var middle_school_enrolling_date = req.body.middle_school_enrolling_date;
+    var middle_school_graduate_date = req.body.middle_school_graduate_date;
+    var high_school = req.body.high_school;
+    var high_school_enrolling_date = req.body.high_school_enrolling_date;
+    var high_school_graduate_date = req.body.high_school_graduate_date;
+    var college = req.body.college;
+    var college_enrolling_date = req.body.college_enrolling_date;
+    var college_graduate_date = req.body.college_graduate_date;
+    var params = {
+        TableName:table,
+        Item:{
+            "studentID": studentID,
+            "name": name,
+            "elementary school": elementary_school,
+            "elementary school enrolling date": elementary_school_enrolling_date,
+            "elementary school graduate date": elementary_school_graduate_date,
+            "middle school": middle_school,
+            "middle school enrolling date": middle_school_enrolling_date,
+            "middle school graduate date": middle_school_graduate_date,
+            "high school": high_school,
+            "high school enrolling date": high_school_enrolling_date,
+            "high school graduate date": high_school_graduate_date,
+            "college": college,
+            "college enrolling date": college_enrolling_date,
+            "college graduate date": college_graduate_date,
+            "last modified time": getDateTime()
+        },
+        ConditionExpression: "#id <> :stuID",
+        ExpressionAttributeNames:{"#id":"studentID"},
+        ExpressionAttributeValues:{
+            ":stuID": studentID
+        }
+    };
+    console.log(params);          
+    
+    dynamodbDoc.put(params, function(err, data) {
         if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
             res.contentType('json');
-            res.send(JSON.stringify({RET: 500, status: "internal error"}));
-            console.log(req.body.id);
-        }
-        else if (result.length != 0) {
+            res.send(JSON.stringify({ RET:500,status: "internal error"}));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
             res.contentType('json');
-            res.send(JSON.stringify({RET: 400, status: "course already exist"}));
-        }
-        else {
-            var id = req.body.id;
-            var name = req.body.name;
-            var location = req.body.location;
-            var desc = req.body.desc;
-            var stu = req.body.stu;
-            var credit = req.body.credit;
-            var time = req.body.time;
-            var approval = req.body.approval;
-            var instructor = req.body.instructor;
-            var website = req.body.website;
-            var dept = req.body.dept;
-            var open = req.body.open;
-            if (id == null || name == null) {
-                res.send(JSON.stringify({ RET:402,status:"wrong JSON format" }));
-            }
-            else {
-                var obj = {
-                    "id": id,
-                    "name": name,
-                    "location": location,
-                    "desc": desc,
-                    "stu": stu,
-                    "credit": credit,
-                    "time": time,
-                    "approval": approval,
-                    "instructor": instructor,
-                    "website": website,
-                    "dept": dept,
-                    "open": open,
-                    "lastModifiedTime": getDateTime()
-                };
-                console.log(obj);
-                db.collection(collectionName).insert(obj, function (err, result) {
-                    if(err) {
-                        res.contentType('json');
-                        res.send(JSON.stringify({ RET:500,status: "internal error"}));
-                    }
-                    else {
-                        logs = JSON.stringify({id:req.body.id,oldData:"None",newData:JSON.stringify(obj),version:getDateTime()});
-                        fs.appendFile(logFile, logs+"\n",
-                            function(err) {
-                                if(err) throw err;
-                            });
-                        res.contentType('json');
-                        res.send(JSON.stringify({ RET:200,status:"success"}));
-                    }
-                })
-            }
+            res.send(JSON.stringify({ RET:200,status:"success"}));
         }
     });
 });
 
 //modify a course's information
-router.put('/courses/:cid', function (req, res, next) {
-    var id = req.params.cid;
+router.put('/students/:sid', function (req, res, next) {
+    var id = req.params.sid;
     var body = req.body;
     body["lastModifiedTime"] = getDateTime();
-    var originData = '';
-    db.collection(collectionName).find({"id": req.params.cid}).toArray(function (err, result) {
+    var params = {
+        TableName:table,
+        Key:{
+            "studentID": id
+        },
+        UpdateExpression: "set body = :r",
+         ExpressionAttributeValues:{
+           ":r":body
+        },
+        ReturnValues:"UPDATED_NEW"
+    };
+    dynamodbDoc.update(params, function(err, data) {
         if (err) {
-            originData = "None.";
-        } else if (result.length == 0) {
-            originData = "None.";
-        }
-        else {
-            originData = result;
+            console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+            res.contentType('json');
+            res.send(JSON.stringify({RET: 500, status: "internal error"}));
+        } else {
+            console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+            res.contentType('json');
+            res.send(JSON.stringify({RET: 200, status: "success"}));
         }
     });
-    db.collection(collectionName).update(
-        {"id": id},
-        {$set: body},
-        function (err, result) {
-            console.log(JSON.stringify(result));
-            if (err) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 500, status: "internal error"}));
-            }
-            else if (result == 0) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 400, status: "course not found"}));
-            }
-            else {
-                logs = JSON.stringify({id:req.params.cid,oldData:JSON.stringify(originData),newData:JSON.stringify(req.body),version:getDateTime()});
-                fs.appendFile(logFile, logs+"\n",
-                    function(err) {
-                        if(err) throw err;
-                    });
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 200, status: "success"}));
-            }
-        });
 });
 
 
 //get a course's information by id
-router.get('/courses/:cid', function(req, res, next) {
-    db.collection(collectionName).find({'id': req.params.cid}).toArray(function (err, result) {
+router.get('/students/:sid', function(req, res, next) {
+    var params = {
+        TableName : table,
+        KeyConditionExpression: "#id = :stuID",
+        ExpressionAttributeNames:{
+            "#id": "studentID"
+        },
+        ExpressionAttributeValues: {
+            ":stuID":req.params.sid
+        }
+    };
+
+    dynamodbDoc.query(params, function(err, data) {
         if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
             res.contentType('json');
             res.send(JSON.stringify({RET: 500, status: "internal error"}));
-        } else if (result.length == 0) {
+        } else if (data.Items.length == 0) {
             res.contentType('json');
             res.send(JSON.stringify({RET: 400, status: "course not found"}));
-        }
-        else {
-            res.json(result);
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(item);
+            });
+            res.json(data.Items[0]);
         }
     });
 });
 
 //get all courses
-router.get('/courses', function(req, res, next) {
-    db.collection(collectionName).find().toArray(function (err, result) {
+router.get('/students', function(req, res, next) {
+    
+    var params = {
+        TableName : table,
+        FilterExpression : 'studentID <> :validID',
+        ExpressionAttributeValues : {
+            ':validID' : 'S'
+        }
+    };
+
+    dynamodbDoc.scan(params, function(err, data) {
         if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
             res.contentType('json');
             res.send(JSON.stringify({RET: 500, status: "internal error"}));
-        }
-        else {
-            res.json(result);
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(item);
+            });
+            res.json(data.Items);
         }
     });
 });
 
 //delete a course
-router.delete('/courses/:cid', function (req, res, next) {
-    if (req.params.cid == 'models')
-        deleteDataModel(req, res, next);
-    else {
-        var originData = '';
-        db.collection(collectionName).find({"id": req.params.cid}).toArray(function (err, result) {
-            if (err) {
-                originData = "None.";
-            } else if (result.length == 0) {
-                originData = "None.";
-            }
-            else {
-                originData = result;
-            }
-        });
-        db.collection(collectionName).remove({"id": req.params.cid}, function (err, result) {
-            console.log(JSON.stringify(result));
-            if (err) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 500, status: "internal error"}));
-            }
-            else if (result == 0) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 400, status: "course not found"}));
-            }
-            else {
-                logs = JSON.stringify({
-                    id: req.params.cid,
-                    oldData: JSON.stringify(originData),
-                    newData: "None",
-                    version: getDateTime()
-                });
-                fs.appendFile(logFile, logs + "\n",
-                    function (err) {
-                        if (err) throw err;
-                    });
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 200, status: "success"}));
-            }
-        });
-    }
-});
-
-
-
-//add students to courses
-router.post('/courses/:cid/students/:sid', function(req, res, next) {
-    var addedStudents = req.params.sid;
-    if (req.params.cid == null || addedStudents == null) {
-        res.send(JSON.stringify({RET: 402, status: "wrong JSON format"}));
-        return;
-    }
-    var findStudent = function (callback) {
-        db.collection(collectionName).find({"id": req.params.cid}).toArray(function (err, result) {
-            if (err) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 500, status: "internal error"}));
-                return;
-            } else if (result.length == 0) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 400, status: "course not found"}));
-                return;
-            }
-            var oriStudents = result[0].stu.split(",");
-            callback(oriStudents);
-        });
-    }
-
-    findStudent(function (oriStudents) {
-        if (oriStudents.indexOf(addedStudents) == -1)
-            oriStudents.push(addedStudents);
-        db.collection(collectionName).update(
-            {"id": req.params.cid},
-            {
-                $set: {
-                    "stu": oriStudents.toString(),
-                    "lastModifiedTime": getDateTime()
-                }
-            },
-            function (err, result) {
-                if (err) {
-                    res.contentType('json');
-                    res.send(JSON.stringify({RET: 500, status: "internal error"}));
-                }
-                else {
-                    res.contentType('json');
-                    res.send(JSON.stringify({RET: 200, status: "success"}));
-                }
-            });
+router.delete('/students/:sid', function (req, res, next) {
+    var params = {
+        TableName : table,
+        Key:{
+            "studentID":req.params.sid
+        }
+    };
+    dynamodbDoc.delete(params, function(err, data) {
+        if (err) {
+            console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+            res.contentType('json');
+            res.send(JSON.stringify({RET: 500, status: "internal error"}));
+        } else {
+            console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+            res.contentType('json');
+            res.send(JSON.stringify({RET: 200, status: "success"}));
+        }
     });
 });
-
-
-//delete students from courses
-router.delete('/courses/:cid/students/:sid', function (req, res, next) {
-    var delStudents = req.params.sid;
-    if (req.params.cid == null || delStudents == null) {
-        res.send(JSON.stringify({ RET:402,status:"wrong JSON format" }));
-        return;
-    }
-    var findStudent = function (callback) {
-        db.collection(collectionName).find({"id": req.params.cid}).toArray(function (err, result) {
-            if (err) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 500, status: "internal error"}));
-                return;
-            } else if (result.length == 0) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 400, status: "course not found"}));
-                return;
-            }
-            var oriStudents = result[0].stu.split(",");
-            if (oriStudents == null) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 500, status: "internal error"}));
-                return;
-            }
-            callback(oriStudents);
-        });
-    }
-
-    findStudent(function (oriStudents) {
-        var index = oriStudents.indexOf(delStudents);
-        if (index != -1)
-            oriStudents.splice(index, 1);
-        db.collection(collectionName).update({"id": req.params.cid},
-            {
-                $set: {
-                    "stu": oriStudents.toString(),
-                    "lastModifiedTime": getDateTime()
-                }
-            },
-            function (err, result) {
-                if (err) {
-                    res.contentType('json');
-                    res.send(JSON.stringify({RET: 500, status: "internal error"}));
-                }
-                else {
-                    res.contentType('json');
-                    res.send(JSON.stringify({RET: 200, status: "success"}));
-                }
-            }
-        );
-    })
-});
-
-//change data model
-router.post('/courses/models', function(req, res, next) {
-    var body = req.body;
-    body["lastModifiedTime"] = getDateTime();
-    db.collection(collectionName).update(
-        {},
-        {$set: body},
-        {upsert:false, multi:true},
-        function(err, result) {
-            if (err) {
-                res.contentType('json');
-                res.send(JSON.stringify({ RET:500,status:"internal error" }));
-            }
-            else {
-                res.contentType('json');
-                res.send(JSON.stringify({ RET:200,status:"success"}));
-            }
-        }
-    );
-});
-
-//delete data model
-function deleteDataModel(req, res, next) {
-    var body = req.body;
-    body["lastModifiedTime"] = getDateTime();
-    db.collection(collectionName).update(
-        {},
-        {$unset: body},
-        {upsert: false, multi: true},
-        function (err, result) {
-            if (err) {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 500, status: "internal error"}));
-            }
-            else {
-                res.contentType('json');
-                res.send(JSON.stringify({RET: 200, status: "success"}));
-            }
-        }
-    );
-};
-
-//data partition
-router.post('/courses/partitions', function (req, res, next) {
-    var result = [];
-    var newS = req.body.newStart.toLowerCase();
-    var newE = req.body.newEnd.toLowerCase();
-    var cursor = db.collection(collectionName).find();
-    var filter = function (cb){
-        cursor.toArray(function(err, array) {
-            async.each(array, function(data, callback) {
-                if (data == null) callback("err");
-                else {
-                    var letter = data.id.substring(1, 2).toLowerCase();
-                    if (letter < newS || letter > newE) {
-                        result.push(data);
-                    }
-                    callback();
-                }
-            }, function(err) {
-                if (err) throw err;
-                else cb(null, result);
-            });
-        });
-    }
-    filter(function(err, result) {
-        for(var i = 0; i < result.length; i++){
-            db.collection(collectionName).remove({"id": result[i].id}, function (err, results) {
-                if (err) {
-                    console.log(JSON.stringify({RET: 500, status: "internal error"}));
-                }
-                else {
-                    console.log(JSON.stringify(results));
-                }
-            });
-        }
-        console.log('result: ');
-        console.log(JSON.stringify(result));
-        res.send(result);
-    });
-});
-
-
-//server status and number of elements in DB
-router.get('/courses/status', function(req, res, next) {
-    db.collection(collectionName).count(function (err, result) {
-        var obj = {
-            "status": "running",
-            "number": result
-        };
-        res.send(obj);
-    });
-});
-
 
 module.exports = router;
